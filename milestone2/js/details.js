52e2e5fcc48168804655bde97b1b20c45570d1f2
KalaPhino <130781392+KalaPhino@users.noreply.github.com>
2025-07-01 21:31:42 -0400

    Update details.js

--- milestone2/js/details.js
+++ milestone2/js/details.js
@@ -1,8 +1,19 @@
+// This page takes API url to get book details with the book ID 
+
+// Makes Ajax request to the google books api for the specific book
+
+
+
 const bookId = new URLSearchParams(window.location.search).get('id');
 
 $.getJSON(`https://www.googleapis.com/books/v1/volumes/${bookId}`, function(data) {
   const v = data.volumeInfo;
-  $('#bookDetails').html(`
+
+
+    //Populating page with book details
+
+
+  $('#bookDetails').html(`                 
     <h2>${v.title}</h2>
     <p><strong>Authors:</strong> ${v.authors?.join(', ')}</p>
     <p><strong>Publisher:</strong> ${v.publisher}</p>
