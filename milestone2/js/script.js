7d28c4ffdc021cc7acb018234c2e03fafaf1e6e8
KalaPhino <130781392+KalaPhino@users.noreply.github.com>
2025-07-01 21:32:03 -0400

    Update script.js

--- milestone2/js/script.js
+++ milestone2/js/script.js
@@ -1,11 +1,11 @@
-const maxResults = 20;
-let currentTerm = "";
+const maxResults = 25;                // Max number of books per page
+let currentTerm = "";                 // Keeps what was placed in the search bar
 
 $('#searchBtn').click(() => {
   currentTerm = $('#searchTerm').val();
   getBooks(0);
 });
-
+                                             // Grabs books from Google Books API
 function getBooks(startIndex) {
   const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(currentTerm)}&startIndex=${startIndex}&maxResults=${maxResults}`;
 
@@ -14,7 +14,7 @@ function getBooks(startIndex) {
     setupPagination(data.totalItems);
   });
 }
-
+                                                    //Book renders on the page 
 function showBooks(books) {
   const container = $('#resultsContainer');
   container.empty();
@@ -33,7 +33,7 @@ function showBooks(books) {
 function setupPagination(totalItems) {
   const pageCount = Math.ceil(Math.min(totalItems, 60) / maxResults);
   const select = $('#pageSelect');
-  select.empty();
+  select.empty();                 // This will reset the dropdown for select
   for (let i = 0; i < pageCount; i++) {
     select.append(`<option value="${i * maxResults}">Page ${i + 1}</option>`);
   }
